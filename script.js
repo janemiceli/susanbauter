dd955bd1540dad3f51f8c06784dbb6de06a7f9c1
Jane Miceli <jane@miceli.me>
2021-12-27 20:26:42 -0700

    fixing jumping text at bottom of the screen

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ var M = {
             COL_WIDTH: 20,
             COL_HEIGHT: 25,
             VELOCITY_PARAMS: {
-                min: 3,
-                max: 7
+                min: 1,
+                max: 2
             },
             CODE_LENGTH_PARAMS: {
                 min: 3,
@@ -109,7 +109,6 @@ var M = {
                             M.codes[i][0].position.y = 0;
                         }
                     }
-
                 }
             }
         },
@@ -121,15 +120,15 @@ var M = {
                 var codeVelocity = (Math.random() * (M.settings.VELOCITY_PARAMS.max - M.settings.VELOCITY_PARAMS.min)) + M.settings.VELOCITY_PARAMS.min, lettersLength = M.letters.length, newLetter = 0;
                 codeLength = M.randomFromInterval(M.settings.CODE_LENGTH_PARAMS.min, M.settings.CODE_LENGTH_PARAMS.max);
                     
-                if ( M.codesCounter%2 == 0 ){
+                //if ( M.codesCounter%2 == 0 ){
                     M.codes[column][0].position = {'x': (column * M.settings.COL_WIDTH), 'y': 0};
                     M.codes[column][0].velocity = codeVelocity;
                     M.codes[column][0].strength = M.codes[column][0].velocity / M.settings.VELOCITY_PARAMS.max;
-                }else{
-                    M.codes[column][0].position = {'x': (column * M.settings.COL_WIDTH), 'y': 0};
-                    M.codes[column][0].velocity = -codeVelocity;
-                    M.codes[column][0].strength = (M.codes[column][0].velocity * -1) / M.settings.VELOCITY_PARAMS.max;
-                }
+                //}else{
+                   // M.codes[column][0].position = {'x': (column * M.settings.COL_WIDTH), 'y': 0};
+                    //M.codes[column][0].velocity = -codeVelocity;
+                    //M.codes[column][0].strength = (M.codes[column][0].velocity * -1) / M.settings.VELOCITY_PARAMS.max;
+                //}
                 M.CheckArray(codeLength, messages, column, lettersLength)
                 M.createCanvii(column);
                 M.codesCounter += 1;
